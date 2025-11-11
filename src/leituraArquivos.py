cb0c89bc6e5864a1136cfe6c8d8c39284975a257
Matheus <matheustole304@gmail.com>
2025-11-11 01:16:38 -0300

    correcoes e adicoes de diferentes algoritmos de coloracao de grafos

--- src/leituraArquivos.py
+++ src/leituraArquivos.py
@@ -6,6 +6,8 @@ def lerDataset():
     print("1 - pequeno.csv")
     print("2 - medio.csv")
     print("3 - grande.csv")
+    print("4 - teste5.csv")
+    print("5 - teste10.csv")
     print("===========================================\n")
 
     datasetEscolhido = int(input("Digite o numero do arquivo do dataset que deseja analisar: "))
@@ -16,9 +18,13 @@ def lerDataset():
         datasetEscolhido = "medio.csv"
     elif(datasetEscolhido == 3):
         datasetEscolhido = "grande.csv"
+    elif(datasetEscolhido == 4):
+        datasetEscolhido = "teste5.csv"
+    elif(datasetEscolhido == 5):
+        datasetEscolhido = "teste10.csv"
 
     grafo = CriarMontarGrafo(datasetEscolhido)
 
-    imprimirDadosGrafo(grafo)
+    # imprimirDadosGrafo(grafo)
 
     return grafo
